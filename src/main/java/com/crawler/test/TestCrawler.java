0662b480cc09cdaaefc99f833437e490b0b9cf55
vkiitr <vkiitr@gmail.com>
2020-01-13 10:08:59 +0530

    added samples

--- src/main/java/com/crawler/test/TestCrawler.java
+++ src/main/java/com/crawler/test/TestCrawler.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 
@@ -23,6 +24,7 @@ public class TestCrawler {
 				links.add(URL);
 
 				Document document = Jsoup.connect(URL).get();
+
 				Elements linksOnPage = document.select("a[href]");
 
 				depth++;
@@ -35,9 +37,37 @@ public class TestCrawler {
 		}
 	}
 
+//	public static void main(String[] args) {
+//		String[] urlList = { 
+//				"https://javarevisited.blogspot.com/2018/09/the-2018-devops-roadmap-your-guide-to-become-DevOps-Engineer.html",
+//				"https://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/",
+//				"https://www.geeksforgeeks.org/minimum-number-of-fibonacci-jumps-to-reach-end/",
+//				"",
+//				"",
+//				""
+//		};
+//
+//		// new TestCrawler().getPageLinks(URL, 0);
+//		try {
+//			Document doc = Jsoup.connect(urlList[2]).get();
+//			System.out.println(doc);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
 	public static void main(String[] args) {
-		new TestCrawler().getPageLinks(
-				"https://javarevisited.blogspot.com/2018/09/the-2018-devops-roadmap-your-guide-to-become-DevOps-Engineer.html",
-				0);
+		String fileName = "src\\main\\samples\\sample1.html";
+
+        try {
+			Document doc = Jsoup.parse(new File(fileName), "utf-8");
+			System.out.println("Title: " + doc.title());
+			System.out.println(doc.wholeText());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
+	
 }
